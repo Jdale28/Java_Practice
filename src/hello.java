e804dace7e271fc9a61dc0ec55d80871c5e3f79c
Jordan Dale <jordan.dale2@gmail.com>
2019-01-14 13:40:37 -0500

    If/Else example

--- src/hello.java
+++ src/hello.java
@@ -15,15 +15,30 @@ public class hello {
 //		System.out.println(ch);
 //		System.out.println(num1);
 //		System.out.println(c2);
-		
+
 //		byte b=10;
 //		b=(byte)(b*b);
 //		System.out.println(b);
-		
-		int num=5;
-		int ans= ++num + num++ + num++;
-		System.out.println(num);
-		System.out.println(ans);
-		
+
+//		int num=5;
+//		int ans= ++num + num++ + num++; // 6 + 6 + 7 --> 8
+//		System.out.println(num);
+//		System.out.println(ans);
+
+//		Else If practice
+
+		int num = 20;
+		if (num < 100 && num >= 10) {
+			System.out.println("It's a two digit number");
+		} else if (num < 1000 && num >= 100) {
+			System.out.println("It's a three digit number");
+		} else if (num < 10000 && num >= 1000) {
+			System.out.println("It's a four digit number");
+		} else if (num < 100000 && num >= 10000) {
+			System.out.println("It's a five digit number");
+		} else {
+			System.out.println("number is not between 1 & 99,999");
+		}
+
 	}
 }
