5078d9a0095990d3f9ead57e1e87c91812bf04cc
Jordan Dale <jordan.dale2@gmail.com>
2019-01-14 13:26:53 -0500

    Type casting promotions Java

--- src/hello.java
+++ src/hello.java
@@ -1,12 +1,24 @@
 
 public class hello {
 	public static void main(String[] args) {
-		int num1=100;
-		float flt1;
-		double dbl;
-		char ch;
-		boolean bl;
-		System.out.println(flt1);
-	
+//		int num1=100;
+//		float flt1;
+//		flt1=(float)13.145F;
+//		long num2=200;
+//		num1=(int)(num1+num2);
+//		double dbl;
+//		char ch,ch1='i';
+//		ch='a';
+//		char c2=(char)(ch+ch1);
+//		num1=ch;
+//		boolean bl;
+//		System.out.println(ch);
+//		System.out.println(num1);
+//		System.out.println(c2);
+		
+		byte b=10;
+		b=(byte)(b*b);
+		System.out.println(b);
+		
 	}
 }
