c4de534e19e2c4f1057aefefdfbe80924551b56d
Jordan Dale <jordan.dale2@gmail.com>
2019-01-14 14:24:39 -0500

    Class practice

---
+++ src/UseDemo.java
@@ -0,0 +1,13 @@
+class Demo {
+	int val;
+}
+public class UseDemo {
+
+	public static void main(String[] args) {
+		Demo obj = new Demo();
+		obj.val = 19000;
+		System.out.println("The value is " + obj.val);
+
+	}
+
+}
