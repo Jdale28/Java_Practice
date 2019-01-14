6b7e12521946c5750220ba8ce1114059f119fa8f
Jordan Dale <jordan.dale2@gmail.com>
2019-01-14 13:45:39 -0500

    Switch practice

--- src/hello.java
+++ src/hello.java
@@ -26,19 +26,47 @@ public class hello {
 //		System.out.println(ans);
 
 //		Else If practice
-
-		int num = 20;
-		if (num < 100 && num >= 10) {
-			System.out.println("It's a two digit number");
-		} else if (num < 1000 && num >= 100) {
-			System.out.println("It's a three digit number");
-		} else if (num < 10000 && num >= 1000) {
-			System.out.println("It's a four digit number");
-		} else if (num < 100000 && num >= 10000) {
-			System.out.println("It's a five digit number");
-		} else {
-			System.out.println("number is not between 1 & 99,999");
+//		int num = 25000;
+//		if (num < 100 && num >= 10) {
+//			System.out.println("It's a two digit number");
+//		} else if (num < 1000 && num >= 100) {
+//			System.out.println("It's a three digit number");
+//		} else if (num < 10000 && num >= 1000) {
+//			System.out.println("It's a four digit number");
+//		} else if (num < 100000 && num >= 10000) {
+//			System.out.println("It's a five digit number");
+//		} else {
+//			System.out.println("number is not between 1 & 99,999");
+//		}
+		
+//		Switch practice
+		int Day = 8;
+		switch (Day) {
+		case 1:
+			System.out.println("Sunday");
+			break;
+		case 2:
+			System.out.println("Monday");
+			break;
+		case 3:
+			System.out.println("Tuesday");
+			break;
+		case 4:
+			System.out.println("Wednesday");
+			break;
+		case 5:
+			System.out.println("Thursday");
+			break;
+		case 6:
+			System.out.println("Friday");
+			break;
+		case 7:
+			System.out.println("Saturday");
+			break;
+		default:
+			System.out.println("Invalid Day");
+			break;
 		}
-
+		
 	}
 }
