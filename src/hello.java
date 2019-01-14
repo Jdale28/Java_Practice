261983e19034f397536ac907f884488bc76bafa8
Jordan Dale <jordan.dale2@gmail.com>
2019-01-14 13:54:27 -0500

    Loop practice Java

--- src/hello.java
+++ src/hello.java
@@ -40,33 +40,54 @@ public class hello {
 //		}
 		
 //		Switch practice
-		int Day = 8;
-		switch (Day) {
-		case 1:
-			System.out.println("Sunday");
-			break;
-		case 2:
-			System.out.println("Monday");
-			break;
-		case 3:
-			System.out.println("Tuesday");
-			break;
-		case 4:
-			System.out.println("Wednesday");
-			break;
-		case 5:
-			System.out.println("Thursday");
-			break;
-		case 6:
-			System.out.println("Friday");
-			break;
-		case 7:
-			System.out.println("Saturday");
-			break;
-		default:
-			System.out.println("Invalid Day");
-			break;
-		}
+//		int Day = 4;
+//		switch (Day) {
+//		case 1:
+//			System.out.println("Sunday");
+//			break;
+//		case 2:
+//			System.out.println("Monday");
+//			break;
+//		case 3:
+//			System.out.println("Tuesday");
+//			break;
+//		case 4:
+//			System.out.println("Wednesday");
+//			break;
+//		case 5:
+//			System.out.println("Thursday");
+//			break;
+//		case 6:
+//			System.out.println("Friday");
+//			break;
+//		case 7:
+//			System.out.println("Saturday");
+//			break;
+//		default:
+//			System.out.println("Invalid Day");
+//			break;
+//		}
 		
+//		Do While Loop
+//		int num=1,sum=0;
+//		do {
+//			sum+=num;
+//			num++;
+//		}while(num<=100);
+//		System.out.println(sum);
+		
+//		While Loop
+//		int num=100,sum=0;
+//		while(num>=1) {
+//			sum+=num;
+//			num--;
+//		}
+//		System.out.println(sum);
+		
+		int sum=0;
+		for (int i=0;i<=100;i++) {
+			sum+=i;
+		}
+		System.out.println(sum);
 	}
 }
