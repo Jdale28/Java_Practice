81a9e00b72ace85d0a754e20cf6537c7cda59f47
Jordan Dale <jordan.dale2@gmail.com>
2019-01-14 14:35:14 -0500

    Object practice Java

--- src/UseDemo.java
+++ src/UseDemo.java
@@ -1,13 +1,17 @@
 class Demo {
-	int val;
+	protected int val;
+	double dval;
+	boolean bval;
+	char cval;
 }
-public class UseDemo {
 
+public class UseDemo {
 	public static void main(String[] args) {
 		Demo obj = new Demo();
-		obj.val = 19000;
+//		obj.val = 19000;
 		System.out.println("The value is " + obj.val);
-
+		System.out.println("The value is " + obj.dval);
+		System.out.println("The value is " + obj.bval);
+		System.out.println("The value is " + obj.cval);
 	}
-
 }
