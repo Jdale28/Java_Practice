7ba9d3f3ee82480d6b7ea3df90bdd697535c7ba4
Jordan Dale <jordan.dale2@gmail.com>
2019-01-17 20:25:45 -0500

    Commented code for additional study

--- src/Dog.java
+++ src/Dog.java
@@ -1,10 +1,12 @@
-
+// Dog is inheriting Animal.java methods and fields
 public class Dog extends Animal {
 
 	public Dog() {
 		
 	}
 	
+	
+	// Overrides Animal grrr sound by adding custom sound for Dog
 	public String makeSound() {
 		return "Woof";
 	}
