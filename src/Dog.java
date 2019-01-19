bf9d216a285fedf372a33522b0aff70f6e7b0b30
Jordan Dale <jordan.dale2@gmail.com>
2019-01-18 22:26:48 -0500

    Adjusted annotations

--- src/Dog.java
+++ src/Dog.java
@@ -8,6 +8,7 @@ public class Dog extends Animal {
 	
 	// Overrides Animal grrr sound by adding custom sound for Dog
 	public String makeSound() {
+		// Can be called from Animal.java to produce the correct sound
 		return "Woof";
 	}
 	
